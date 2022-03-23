021c7801586a82b2e02d11aa61266b52630e3daa
KAKPONOTAM <sabitovdanill@icloud.com>
2022-03-23 13:51:25 +0300

    123

--- weatherApp/Controllers/WeatherDataViewController.swift
+++ weatherApp/Controllers/WeatherDataViewController.swift
@@ -58,9 +58,18 @@ class WeatherDataViewController: UIViewController {
         addSubview()
         setupConstraints()
         setupTableView()
+        
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Test Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
     
-    
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        let numbers = [0]
+        let _ = numbers[1]
+    }
     
     private func setupTableView() {
         weatherInfoTableView.delegate = self
